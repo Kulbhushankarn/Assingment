cb8302682239b0046cc69f96d902aa0393c834f7
Kulbhushan <karnkulbhushan@gmail.com>
2023-12-06 23:25:41 +0530

    new commit

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,47 +1,67 @@
 ﻿using System;
-using System.Text;
 
-namespace practice
+class Program
 {
-    internal class Intern
+    static void Main()
     {
-        public static void Main()
+        Console.WriteLine("Sample Input code");
+        Console.WriteLine("Press 'I' for addition and 'O' for subtraction. Press 'Q' to quit.");
+
+        char operationSymbol = '+';
+
+        while (true)
         {
-            string s1 = "one";
-            string s2 = new StringBuilder().Append("on").Append("e").ToString();
-            string s3 = String.Intern(s2);
-            Console.WriteLine($"s1 == {s1}");
-            Console.WriteLine($"s2 == {s2}");
-            Console.WriteLine($"s3 == {s3}");
-            Console.WriteLine($"Is s2 the same reference as s1?: {(Object)s2 == (Object)s1}");
-            Console.WriteLine($"Is s3 the same reference as s1?: {(Object)s3 == (Object)s1}");
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
 
-            string s4 = "two";
-            string s5 = new StringBuilder().Append("two").ToString();
-            string s6 = String.Intern(s5);
-            Console.WriteLine($"s4 == {s4}");
-            Console.WriteLine($"s5 == {s5}");
-            Console.WriteLine($"s6 == {s6}");
-            Console.WriteLine($"Is s5 the same reference as s4?: {(Object)s5 == (Object)s4}");
-            Console.WriteLine($"Is s6 the same reference as s4?: {(Object)s6 == (Object)s4}");
+            if (keyInfo.Key == ConsoleKey.Q)
+            {
+                break;
+            }
 
-            string s7 = "three";
-            string s8 = new StringBuilder().Append("three").ToString(); // Modified to match s7
-            string s9 = String.Intern(s8);
-            Console.WriteLine($"s7 == {s7}");
-            Console.WriteLine($"s8 == {s8}");
-            Console.WriteLine($"s9 == {s9}");
-            Console.WriteLine($"Is s8 the same reference as s7?: {(Object)s8 == (Object)s7}"); // Corrected comparison
-            Console.WriteLine($"Is s9 the same reference as s7?: {(Object)s9 == (Object)s7}");
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.I:
+                    operationSymbol = '+';
+                    break;
 
-            string s10 = "four";
-            string s11 = new StringBuilder().Append("four").ToString();
-            string s12 = String.Intern(s11);
-            Console.WriteLine($"s10 == {s10}");
-            Console.WriteLine($"s11 == {s11}");
-            Console.WriteLine($"s12 == {s12}");
-            Console.WriteLine($"Is s11 the same reference as s10?: {(Object)s11 == (Object)s10}");
-            Console.WriteLine($"Is s12 the same reference as s10?: {(Object)s12 == (Object)s10}");
+                case ConsoleKey.O:
+                    operationSymbol = '-';
+                    break;
+
+                default:
+                    
+                    Console.WriteLine($"Ignoring key '{keyInfo.Key}'. Press 'I', 'O', or 'Q'.");
+                    continue;
+            }
+
+            Console.WriteLine($"Operation set to {operationSymbol}. Press 'Enter' to perform the operation.");
+
+            
+            while (true)
+            {
+                ConsoleKeyInfo enterKeyInfo = Console.ReadKey(true);
+
+                if (enterKeyInfo.Key == ConsoleKey.Enter)
+                {
+                   
+                    int operand1 = 5;  
+                    int operand2 = 3;  
+
+                    int result = (operationSymbol == '+') ? operand1 + operand2 : operand1 - operand2;
+
+                    Console.WriteLine($"Result: {result}");
+                    break;
+                }
+                else if (enterKeyInfo.Key == ConsoleKey.Q)
+                {
+                    
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine($"Ignoring key '{enterKeyInfo.Key}'. Press 'Enter' or 'Q'.");
+                }
+            }
         }
     }
 }
